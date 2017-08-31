7008f69f57cb98b85d350aede4b26565d3081ff8
Anne Castrillon <anne.castrillon@gmail.com>
2017-08-30 21:02:06 -0700

    Initial Commit

---
+++ blogger-mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blogger-mobile
+//
+//  Created by Anne Castrillon on 8/30/17.
+//  Copyright © 2017 blogger_mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
